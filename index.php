8303e7075c77d4afd47bf4ce8cf5635a40c2c71c
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2018-02-14 21:12:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php
 
 echo "Hello World";
+  $db_user_name = 'startupwala_user';
+    $db_password = 'irdi@123';
+     $db_name = 'startup_wala';
 
-$ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "http://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
-}
-curl_close ($ch);
+ function connection_db()
+    {
+        return mysqli_connect('www.startupwala.in', $db_user_name, $db_password, $db_name);
+    }
+
+       $var =  connection_db();
+
+echo $var;
+
 
 ?>
