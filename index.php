c50514279127959756094fe93d10dfe35ec0b1f7
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2018-02-14 21:15:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
  error_reporting(E_ALL);
      ini_set('display_errors', 1);
 echo "Hello World";
-  $db_user_name = 'startupwala_user';
-    $db_password = 'irdi@123';
-     $db_name = 'startup_wala';
-
+ 
 
 
  function connection_db()
     {
+   $db_user_name = 'startupwala_user';
+    $db_password = 'irdi@123';
+     $db_name = 'startup_wala';
+
         return mysqli_connect('www.startupwala.in', $db_user_name, $db_password, $db_name);
     }
 
