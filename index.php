12004eb860e6a4b10d06d692b355e20628fb015a
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2018-02-14 20:27:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,15 @@
 
 echo "Hello World";
 
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "http://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+
 ?>
