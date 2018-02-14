446a074120e70b365f3a53bd03de3bb8bb6a8627
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2018-02-14 21:42:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ echo "Hello World";
  function connection_db()
     {
    $db_user_name = 'startupwala_user';
-    $db_password = '';
+    $db_password = 'irdi@123';
      $db_name = 'startup_wala';
 
         return mysqli_connect('startupwala.cqyhn6lgmvjy.ap-south-1.rds.amazonaws.com:3306', $db_user_name, $db_password, $db_name);
