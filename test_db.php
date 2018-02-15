7364823a6da0650cb0e32936fb0a26befdafdf75
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2018-02-15 11:53:25 +0530

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -4,5 +4,18 @@ require_once("heroku_postgres_database.php");
       
       var_dump($herokupostgrsdatabse);
 
+ $query = "CREATE TABLE trademark_search_reports (
+  report_id              SERIAL PRIMARY KEY,
+  brand_name           VARCHAR(120) NOT NULL,
+  trademark_class  integer(3) NULL,
+  trade_description varchar(200) NULL,
+  search_report VARCHAR(30) NULL,
+  meaning_of_report VARCHAR(150) NULL,
+  what_next varchar(15) NULL,
+  aws_identifier varchar(50) NULL
+)";
+        $data = $herokupostgrsdatabse->query($query);
+
+
 
 ?>
